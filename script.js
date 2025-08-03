8547774e25d88303ddbbfdd4a17115ad69cd6fe1
god-wendigo <kaungsitt27@gmail.com>
2025-08-03 11:42:20 +0630

    Initial Commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('downloadBtn').addEventListener('click', () => {
+  const apkUrl = "https://github.com/god-wendigo/aries_translation/releases/download/v1.0.0/Aries.Translation.v1.apk";
+  window.location.href = apkUrl;
+});
