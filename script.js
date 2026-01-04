30498a7855389b4f9cf10003b18f39fa4276b605
god-wendigo <kaungsitt27@gmail.com>
2026-01-04 19:24:18 +0630

    verion 2.0.0

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById('downloadBtn').addEventListener('click', () => {
-  const apkUrl = "https://github.com/god-wendigo/aries_app_download/releases/download/v1.0.0/Aries.Translation.v1.apk";
+  const apkUrl = "https://github.com/god-wendigo/aries_app_download/releases/download/v2.0.0/Aries.Translation.v2.0.0.apk";
   window.location.href = apkUrl;
 });
